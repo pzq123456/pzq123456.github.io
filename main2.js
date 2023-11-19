49c9c570f3812971aa98f8d760c070a06b8889cc
panzhiqing <1812673119@qq.com>
2023-11-19 21:08:56 +0800

    重写渲染代码ing

--- main2.js
+++ main2.js
@@ -3,7 +3,7 @@ import { fillNavBar } from './helpers/navBar.js';
 
 import { metalist } from './blogs/meta.js'; // metalist is a list of blog metadata
 import { createCanvas, animationEngine, eventEngine} from './src/Terminal/view.js';
-import { drawTData,drawMouse,smartDrawMouse } from './src/Terminal/renderer.js';
+import { drawTData2,drawMouse,smartDrawMouse } from './src/Terminal/renderer.js';
 import { Line, TerminalData } from './src/Terminal/data.js';
 import {parseLine, run} from './src/Terminal/interpreter.js';
 import { blockStyle,blockStyle2,blockStyle3,blockStyle4,blockStyle5 } from './src/Terminal/defaultStyle.js';
@@ -13,3 +13,34 @@ import {initPage2} from './helpers/init.js';
 
 // Terminal === 部分
 let myCanvas = createCanvas(document.getElementById('terminal'), window.innerWidth * 0.81, 300);
+let myHistory =`pzq123456.github.io%> cd /blogs/Blog1.md`;
+let Tdata = TerminalData.fromString(myHistory);
+let wholeStyle = {
+    'background-color': 'black',
+    // 行间距
+    'line-interval': '10px',
+}
+/**
+ * 自定义样式 根据block的内容
+ * @param {Block} block 
+ */
+function getStyle(block){
+    if(block.contains("%")){
+        // console.log('pzq');
+        return blockStyle;
+    }else if(block.equals("cd") || block.equals('ls') || block.equals('cat') || block.equals('clear') || block.equals('help')){
+        return blockStyle3;
+    }else if(
+        block.contains('/') ||  block.contains(`path`)
+    ){
+        return blockStyle4;
+    }else if(block.contains('202') || block.contains(`date`)){
+        return blockStyle5;
+    }
+    else{
+        return blockStyle2;
+    }
+}
+// draw Tdata
+drawTData2(myCanvas, Tdata, 0, 40, wholeStyle, getStyle,0);
+console.log(Tdata);

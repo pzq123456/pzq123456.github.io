dc32f519c2830e7519196f91c090480a02a8a160
panzhiqing <1812673119@qq.com>
2023-12-03 19:37:23 +0800

    123

---
+++ blogs/meta.js
@@ -0,0 +1,64 @@
+export const metalist = [
+    {
+        "title" : "README",
+        "path" : "/README.md",
+        "date" : "2023-10-6",
+        "tag" : "info",
+        "command": "cd"
+    },
+    {
+        "title" : "Blog0",
+        "path" : "blogs/Blog0.md",
+        "date" : "2023-10-14",
+        "tag" : "info",
+        "command": "cd"
+    },
+    {
+        "title" : "Blog1",
+        "path" : "blogs/Blog1.md",
+        "date" : "2023-10-15",
+        "tag" : "front-end",
+        "command": "cd"
+    },
+    {
+        "title" : "Blog2",
+        "path" : "blogs/Blog2.md",
+        "date" : "2023-10-23",
+        "tag" : "info",
+        "command": "cd"
+    },
+    {
+        "title" : "Blog3",
+        "path" : "blogs/Blog3.md",
+        "date" : "2023-10-23",
+        "tag" : "front-end",
+        "command": "cd"
+    },
+    {
+        "title" : "Blog4",
+        "path" : "blogs/Blog4.md",
+        "date" : "2023-10-24",
+        "tag" : "front-end",
+        "command": "cd"
+    },
+    {
+        "title" : "tmp0",
+        "path" : "blogs/tmp0.md",
+        "date" : "2023-10-26",
+        "tag" : "tmp",
+        "command": "cd"
+    }
+]
+
+const tag = [
+    "info",
+    "front-end",
+    "back-end",
+    "algorithm",
+    "data-structure",
+    "machine-learning",
+    "math",
+    "3d",
+    "tmp",
+    "*toy",
+]

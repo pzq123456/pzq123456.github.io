f94b011c24d949adc631ef2639da20b4af5663ce
PanZhiQing <panzhiqing15@gmail.com>
2024-12-24 00:04:51 +0800

    fix: chrome 浏览器换行符重复问题。

--- example/main.js
+++ example/main.js
@@ -3,7 +3,26 @@ const editableLayer = document.querySelector('.editable-layer');
 const errorMessagesDiv = document.getElementById('errorMessages');
 const myCanvas = document.getElementById('myCanvas');
 
+const container = document.querySelector('.editor-container');
 
+function init(){
+  // 自动获取高亮图层的 字体及行间距 并将所有样式应用到可编辑层
+  const computedStyle = window.getComputedStyle(highlightLayer);
+  editableLayer.style.fontFamily = computedStyle.fontFamily;
+  editableLayer.style.fontSize = computedStyle.fontSize;
+  editableLayer.style.lineHeight = computedStyle.lineHeight;
+  editableLayer.style.whiteSpace = 'pre-wrap';
+
+  // 获取 container 的宽高
+  const width = container.offsetWidth;
+  const height = container.offsetHeight;
+
+  // 重设 canvas 元素宽高
+  myCanvas.width = width;  // 设置 canvas 的宽度
+  myCanvas.height = height; // 设置 canvas 的高度
+}
+
+init();
 
 // 过滤粘贴内容的函数，去除样式
 function sanitizePaste(content) {
@@ -15,24 +34,21 @@ function sanitizePaste(content) {
 
 // 更新高亮显示
 function updateHighlight() {
+  // 获取可编辑层内容并保留换行符
   const content = editableLayer.innerText;
-  const highlighted = hljs.highlightAuto(content, ['javascript', 'html', 'css', 'markdown', 'bash']);
+  const highlighted = hljs.highlightAuto(content, ['javascript', 'html', 'css', 'markdown', 'bash']); // 高亮代码
   highlightLayer.innerHTML = highlighted.value;
 }
 
-function updateCanvas(){
-  // clear canvas
-  const ctx = myCanvas.getContext('2d');
-  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-  ctx.fillStyle = 'blue';
-  const randomX = Math.floor(Math.random() * 400);
-  const randomY = Math.floor(Math.random() * 400);
-  ctx.fillRect(randomX, randomY, 50, 50);
+
+
+// 使用 html2canvas 渲染 highlighLayer 到 canvas
+function updateCanvas() {
+  // const ctx = myCanvas.getContext('2d'); // 获取 canvas 上下文
 }
 
 function render() {
-  debounce(updateHighlight, 100)();
-  // debounce(updateCanvas, 50)();
+  debounce(updateHighlight, 1000)();
 }
 
 // animate loop
@@ -40,10 +56,6 @@ function animate() {
   render();
   requestAnimationFrame(animate);
 }
-
-animate();
-
-
 
 // 同步滚动条
 function syncScroll() {
@@ -59,7 +71,11 @@ editableLayer.addEventListener('paste', (event) => {
 });
 
 // 监听输入事件
-// editableLayer.addEventListener('input', debouncedUpdateHighlight);
+editableLayer.addEventListener('input', () => {
+  updateHighlight();
+  // updateCanvas();
+});
+
 editableLayer.addEventListener('scroll', syncScroll);
 
 // 初始化
